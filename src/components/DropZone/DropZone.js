2dbaa109f7bb70b6edea571b461254cdaa4f5eb6
Sanyam Gupta <sanyamgupta@Sanyams-MacBook-Pro.local>
2021-05-13 22:24:46 -0700

    removed useCallback from Dropzone

--- src/components/DropZone/DropZone.js
+++ src/components/DropZone/DropZone.js
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 
 import styles from './DropZone.module.css';
 import {useDropzone} from 'react-dropzone';
@@ -8,24 +8,22 @@ const DropZone = (props) => {
 
     const {onSetMetadata} = props;
     
-    const onDrop = useCallback((acceptedFiles) => {
+    const onDrop = (acceptedFiles) => {
         const data = acceptedFiles[0];
         let modifiedData = null;
 
         if (data) {
-
             modifiedData =  {
                 name : data.name,
                 size : data.size,
                 lastModifiedDate : data.lastModifiedDate.toString(),
                 type : data.type
-            }     
-
+            }            
         }
 
         onSetMetadata(modifiedData);
 
-    }, []);
+    };
 
     const {getRootProps, getInputProps} = useDropzone({onDrop, maxFiles : 1});
 
